ce3d5945abc0ab7a857a8d44d1316dd2fb95b512
jackson <s97403002a@gmail.com>
2025-02-13 14:57:11 +0800

    add curd

--- Movies.Api/Controllers/MoviesController.cs
+++ Movies.Api/Controllers/MoviesController.cs
@@ -28,4 +28,51 @@ public class MoviesController : ControllerBase
         await _movieRepository.CreateAsync(movie);
         return Ok();
     }
+
+    [HttpGet("api/movies")]
+    public async Task<IActionResult> GetAllAsync()
+    {
+        var movies = await _movieRepository.GetAllAsync();
+        return Ok(movies);
+    }
+
+    [HttpGet("api/movies/{id}")]
+    public async Task<IActionResult> Get([FromRoute] Guid id)
+    {
+        var movie = await _movieRepository.GetByIdAsync(id);
+        if (movie == null)
+        {
+            return NotFound();
+        }
+        return Ok(movie);
+    }
+    
+    [HttpPut("api/movies/{id}")]
+    public async Task<IActionResult> UpdateAsync([FromRoute] Guid id, [FromBody] UpdateMovieRequest request)
+    {
+        var movie = new Movie()
+        {
+            Id = id,
+            Title = request.Title,
+            YearOfRelease = request.YearOfRelease,
+            Genres = request.Genres.ToList()
+        };
+        var update = await _movieRepository.UpdateAsync(movie);
+        if (!update)
+        {
+            return NotFound();
+        }
+        return Ok();
+    }
+    
+    [HttpDelete("api/movies/{id}")]
+    public async Task<IActionResult> DeleteAsync([FromRoute] Guid id)
+    {
+        var delete = await _movieRepository.DeleteByIdAsync(id);
+        if (!delete)
+        {
+            return NotFound();
+        }
+        return Ok();
+    }
 }
